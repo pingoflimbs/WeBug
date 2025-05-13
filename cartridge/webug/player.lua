30180933d130eec20613de9bec9099cb8e169967
cmyu <cmyu@imagis.co.kr>
2025-05-13 15:25:32 +0900

    좌우로만 움직이도록 변경, 윗방향키 눌렀을때 커지게, fire버튼 눌렀을때 색깔랜덤으로 변하게 변경, jump버튼 눌렀을때 위로 올라가게 변경

--- cartridge/webug/player.lua
+++ cartridge/webug/player.lua
@@ -6,25 +6,29 @@
 -- 플레이어 객체를 정의하는 모듈입니다.
 --------------------------------------------------------------------------------
 
-function init_player(y, play_no)
+function init_player(x, play_no)
     local player = 
     {        
-        y = y,
+        x = x,
+        y = POS.BOTTOM,
         size = 3,
         life = 3,
         speed = 2,
         play_no = play_no,
+        color = 3,
     }
 
     function player:draw()
-        circfill(self.y, 120, self.size, 3)
+        circfill(self.x, self.y, self.size, self.color)
     end
 
     function player:update()
-        if btn(BTN.LEFT, play_no) then self.y -= self.speed end
-        if btn(BTN.RIGHT, play_no) then self.y += self.speed end
+        if btn(BTN.LEFT, play_no) then self.x -= self.speed end
+        if btn(BTN.RIGHT, play_no) then self.x += self.speed end
         if btn(BTN.DOWN, play_no) then self.size -= self.speed end
         if btn(BTN.UP, play_no) then self.size += self.speed end
+        if btn(BTN.JUMP, play_no) then self.y -= self.speed end
+        if btn(BTN.FIRE, play_no) then self.color = flr(rnd(8)) end
     end
 
     return player
