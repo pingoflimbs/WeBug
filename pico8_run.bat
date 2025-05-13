e8fef773fe95a6813bfa74757c050e743dcc7a5c
cmyu <cmyu@imagis.co.kr>
2025-05-13 14:13:39 +0900

    -run 파라미터가 제대로 동작하지않아 삭제

--- pico8_run.bat
+++ pico8_run.bat
@@ -2,5 +2,5 @@
 
 :nohide
 cd /d %~dp0
-start "" pico-8\pico8.exe -root_path "cartridge" -windowed 1 -window_size 4 -run webug.p8
+start "" pico-8\pico8.exe -root_path "cartridge" -windowed 1 -window_size 4
 exit
